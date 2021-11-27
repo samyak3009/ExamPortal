45a78a099691b62b35d5e07c5e268ee91da4aa13
samyak3009 <samyak3009@gmail.com>
2021-11-27 08:48:01 +0530

    final frontend

--- frontend/src/app/components/services/BaseUrl.js
+++ frontend/src/app/components/services/BaseUrl.js
@@ -2,7 +2,7 @@
   'use strict';
 angular
     .module('minotaur').factory('BaseUrl',function(){
-	var Str='https://adf3-103-155-209-248.ngrok.io/';
+	var Str='https://microsoftback.herokuapp.com/';
 	// var Str='https://tech.kiet.edu/api/hrms/';
 	return {
 		RetBaseUrl:function(){ return Str; }
